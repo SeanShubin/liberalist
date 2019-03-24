9af886de38c65bb3dac5f9220fbd06b69ccfa05c
SeanShubin <seanshubin@gmail.com>
2019-03-23 23:05:12 -0700

    tab behavior

--- prototype/tabs/index.js
+++ prototype/tabs/index.js
@@ -20,49 +20,58 @@ const urlToPageName = (url) => {
 const model = {
     'a': {
         tabBars: [[
-            {title: 'A Title', active: true},
-            {title: 'C Title', active: false},
-            {title: 'E Title', active: false}]],
+            {title: 'A Title', page: 'a', active: true},
+            {title: 'C Title', page: 'c', active: false},
+            {title: 'E Title', page: 'e', active: false}]],
         fragment: 'a.html'
     },
     'c': {
         tabBars: [
             [
-                {title: 'A Title', active: false},
-                {title: 'C Title', active: true},
-                {title: 'E Title', active: false}
+                {title: 'A Title', page: 'a', active: false},
+                {title: 'C Title', page: 'c', active: true},
+                {title: 'E Title', page: 'e', active: false}
             ],
             [
-                {title: 'C Title', active: true},
-                {title: 'D Title', active: false}
+                {title: 'C Title', page: 'c', active: true},
+                {title: 'D Title', page: 'd', active: false}
             ]],
         fragment: 'b/c.html'
     },
     'd': {
         tabBars: [
             [
-                {title: 'A Title', active: false},
-                {title: 'C Title', active: true},
-                {title: 'E Title', active: false}
+                {title: 'A Title', page: 'a', active: false},
+                {title: 'C Title', page: 'c', active: true},
+                {title: 'E Title', page: 'e', active: false}
             ],
             [
-                {title: 'C Title', active: false},
-                {title: 'D Title', active: true}
+                {title: 'C Title', page: 'c', active: false},
+                {title: 'D Title', page: 'd', active: true}
             ]],
         fragment: 'b/d.html'
     },
     'e': {
         tabBars: [
             [
-                {title: 'A Title', active: false},
-                {title: 'C Title', active: false},
-                {title: 'E Title', active: true}
+                {title: 'A Title', page: 'a', active: false},
+                {title: 'C Title', page: 'c', active: false},
+                {title: 'E Title', page: 'e', active: true}
             ]],
         fragment: 'e.html'
     }
 };
 
 const createDiv = () => document.createElement('div');
+const createUl = () => document.createElement('ul');
+const createLi = () => document.createElement('li');
+const createLink = (args) => {
+    const {href, text} = args;
+    const a = document.createElement('a');
+    a.href = href;
+    a.text = text;
+    return a;
+};
 
 const renderParagraph = (text) => {
     const p = document.createElement('p');
@@ -71,7 +80,18 @@ const renderParagraph = (text) => {
 };
 
 const renderTabBar = (tabBar) => {
-    return renderParagraph(JSON.stringify(tabBar));
+    const ul = createUl();
+    const appendTab = (tab) => {
+        const li = createLi();
+        const link = createLink({href: '?page=' + tab.page, text: tab.title});
+        li.appendChild(link);
+        if (tab.active) {
+            li.classList.add('active');
+        }
+        ul.appendChild(li);
+    };
+    tabBar.forEach(appendTab);
+    return ul;
 };
 
 const renderFragment = async (fragmentName) => {
@@ -81,10 +101,11 @@ const renderFragment = async (fragmentName) => {
 const renderPageModel = async (pageModel) => {
     const {tabBars, fragment} = pageModel;
     const div = createDiv();
-    tabBars.forEach((tabBar) => {
+    const appendTabBar = (tabBar) => {
         const renderedTabBar = renderTabBar(tabBar);
         div.appendChild(renderedTabBar);
-    });
+    };
+    tabBars.forEach(appendTabBar);
     const renderedFragment = await renderFragment(fragment);
     div.appendChild(renderedFragment);
     return div;
